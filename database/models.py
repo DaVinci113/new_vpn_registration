fedc7b96dc5b5abc4d5946d2c64432ddaa2ffabf
DaVinci113 <maxkgd@mail.ru>
2026-09-21 15:50:13 +0200

    Добавил создание таблицы

---
+++ database/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from sqlalchemy import DateTime, func
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    telegram_id: Mapped[int]
+    free_plan: Mapped[bool] = mapped_column(default=True)
+    start_free_plan: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    end_free_plan: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    start_plan: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    end_plan: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    wallet: Mapped[int] = mapped_column(default=0)
+    connected_devices: Mapped[int] = mapped_column(default=0)
