4aa33a9824751319cfba26ea0afd2d6ea51e46dc
DaVinci113 <maxkgd@mail.ru>
2026-09-21 15:51:07 +0200

    Добавил aiosqlite

---
+++ database/db.py
@@ -0,0 +1,22 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator, Annotated
+
+from pydantic import with_config
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+url = 'sqlite+aiosqlite:///db.sqlite'
+engine = create_async_engine(url=url, future=True)
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
+
+@asynccontextmanager
+async def session_scope() -> AsyncIterator[AsyncSession]:
+    async with async_session_maker() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
+
+async def get_session():
+    async with session_scope() as session:
+        yield session
